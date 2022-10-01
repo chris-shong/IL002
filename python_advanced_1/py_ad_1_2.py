fd3039ff140aaf48b586492718b9eda0875dc346
chris-shong <csylee94@gmail.com>
2022-10-01 21:15:40 +0900

    Create py_ad_1_2.py

---
+++ python_advanced_1/py_ad_1_2.py
@@ -0,0 +1,65 @@
+"""
+Chapter1 Advanced(1) - Lambda, Reduce, Map, Filter Functions
+Keyword - lambda, map, filter, reduce
+
+"""
+"""
+lambda 장점: 익명, 힙 영역 사용 즉시 소멸, pythonic?, 파이썬 가비지 컬렉션(Count=0), 일회성
+일반함수: 재사용성 위해 메모리 저장
+시퀀스형 전처리에 Reduce, Map, Fitler 주로 사용
+
+"""
+
+# Ex1
+cul = lambda a, b, c: a * b + c
+
+print('Ex1 >', cul(10, 15, 20))
+
+# Ex2
+digits1 = [x * 10 for x in range(1, 11)]
+print('Ex2 >', digits1)
+
+result = list(map(lambda i: i ** 2, digits1))
+print('Ex2 >', result)
+
+
+def also_square(nums):
+    def double(x):
+        return x ** 2
+
+    return map(double, nums)
+
+
+print('Ex2 >', list(also_square(digits1)))
+
+# Ex3
+digits2 = list(range(1, 10))
+
+result = list(filter(lambda x: x % 2 == 0, digits2))
+print('Ex3 >', result)
+
+
+def also_evens(nums):
+    def is_even(x):
+        return x % 2 == 0
+
+    return filter(is_even, nums)
+
+
+print('Ex3 >', list(also_evens(digits2)))
+
+# Ex4
+from functools import reduce
+
+digits3 = [x for x in range(1, 101)]
+
+result = reduce(lambda x, y: x + y, digits3)
+
+print('Ex4 >', result)
+
+def also_add(nums):
+    def add_plus(x, y):
+        return x + y
+    return reduce(add_plus, nums)
+
+print('Ex4 >', also_add(digits3))
